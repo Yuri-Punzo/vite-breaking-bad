eb8a78e6d823d0f0039a6967393c6a2e01402540
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-14 15:11:12 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- 
+Descrizione:
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato. Al caricamento della pagina, effettuate una chiama ajax all'API di Breaking Bad: https://www.breakingbadapi.com/api/characters e con i dati restituiti, stampate una card per ogni personaggio.
+
+Bonus:
+Creare un componente loader da visualizzare fintantoché i risultati non sono pronti. -->
+<script>
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
